ec8628f424d096f1f8cbed2d9e41851109b3d4d5
zhoucan <Jax.Zhou@luxshare-ict.com>
2019-07-19 15:13:20 +0800

    修复注释bug

--- RushCommand/RushCommand.py
+++ RushCommand/RushCommand.py
@@ -29,38 +29,3 @@ class rushCommand(sublime_plugin.TextCommand):
             print(str(out,encoding='utf-8'))
         else:
             return
-
-
-
-
-class zsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        #当前视图
-        view = self.view
-        #当前选择的区域
-        sels = view.sel()
-        selContent = ''
-        if len(sels) > 0 :
-            #获取以一个选中区域
-            sels = sels[0]
-        #获取选中区域内容
-        regionStr = view.substr(sels)
-        #重新拼接字符串--前面插入一个tab
-        for s in regionStr.split('\n'):
-            selContent += '\t' + s + '\n';
-        #剪切掉当前选中的内容
-        view.run_command('cut')
-        #获取当前时间
-        curtime = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
-        content = ' #  @athor #作者\n  # @desc #描述\n  # @date ' + curtime + '\n'
-        content = content + selContent;
-        view.insert(edit,0,content)
-        #清空剪切板
-        sublime.set_clipboard('')       
-
-
- 
-        	
-
-
-
