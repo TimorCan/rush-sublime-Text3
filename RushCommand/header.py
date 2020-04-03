67121a7d7082ed7b41f28b75923176cb5a9a96c0
zhoucan <Jax.Zhou@luxshare-ict.com>
2020-04-03 14:39:46 +0800

    new version

---
+++ RushCommand/header.py
@@ -0,0 +1,11 @@
+import datetime, getpass
+
+import sublime, sublime_plugin
+
+class AddDateTimeStampCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.run_command("insert_snippet", { "contents": "%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") } )
+
+class UserNameCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.run_command("insert_snippet", { "contents": getpass.getuser() } )
